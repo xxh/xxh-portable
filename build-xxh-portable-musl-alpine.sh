df3b639fdcc61eb7fda9083418d5816d67d43600
Andy Kipp <anki-code@users.noreply.github.com>
2021-03-23 08:05:16 +0300

    Delete build-xxh-portable-musl-alpine.sh

--- build-xxh-portable-musl-alpine.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-docker build . -f xxh-portable-musl-alpine.Dockerfile -t xxh/xxh-portable-musl-alpine  #--no-cache --force-rm
-docker run --rm -v `pwd`/result:/result xxh/xxh-portable-musl-alpine
